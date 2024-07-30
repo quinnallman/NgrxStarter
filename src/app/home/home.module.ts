76e57235079f5b52ba9195b3e266429ca6dadee8
Greg Roberts <groberts@gmail.com>
2024-07-30 18:34:54 -0400

    Provide the http client to the home component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import {BrowserModule} from "@angular/platform-browser";
 import {ReactiveFormsModule} from "@angular/forms";
 import {HomeComponent} from "./home.component";
 import {CommonModule} from "@angular/common";
+import { provideHttpClient } from "@angular/common/http";
 
 @NgModule({
     imports: [
@@ -15,7 +16,10 @@ import {CommonModule} from "@angular/common";
     ],
     exports: [
         HomeComponent
-    ]
+    ],
+    providers: [
+        provideHttpClient(),
+    ],
 })
 export class HomeModule {
 }
